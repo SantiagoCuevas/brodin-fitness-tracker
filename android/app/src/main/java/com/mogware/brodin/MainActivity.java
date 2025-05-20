53617123d2528ab0e16fb12b8e911c336a80af38
Santiago Cuevas <santiagocuevas77@gmail.com>
2025-05-19 22:18:28 -0600

    capacitor setup

---
+++ android/app/src/main/java/com/mogware/brodin/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mogware.brodin;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mogware.brodin',
+  appName: '\x15Brodin Fitness',
+  webDir: 'dist'
+};
+
+export default config;
