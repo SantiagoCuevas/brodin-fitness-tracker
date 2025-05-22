11d0f28b575608ffef55ed01f963cbfab6fb9097
Santiago Cuevas <santiagocuevas77@gmail.com>
2025-05-22 00:03:22 -0600

    set up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
-import { useState } from "react";
-import { MantineProvider, Title } from "@mantine/core";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { MantineProvider } from "@mantine/core";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import "@mantine/core/styles.css";
+import { Home } from "./pages/Home";
+import { Info } from "./pages/Info";
+import { Settings } from "./pages/Settings";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <MantineProvider>
-      <div>
-        <Title order={1}>This is h1 title</Title>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-2xl">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </BrowserRouter>
     </MantineProvider>
   );
 }
