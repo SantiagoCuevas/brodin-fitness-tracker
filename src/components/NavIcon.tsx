1047a3b8f9fcf679429fc8fd9ece1d4e2837f212
ValorousPete <valorouspete@gmail.com>
2025-05-26 17:09:00 -0600

    updated navbar

---
+++ src/components/NavIcon.tsx
@@ -0,0 +1,22 @@
+import { ActionIcon } from '@mantine/core';
+import { Icon, IconProps } from '@tabler/icons-react';
+import { FC, ForwardRefExoticComponent } from 'react';
+import { NavLink } from 'react-router';
+
+interface NavIconProps {
+  to: string;
+  Icon: ForwardRefExoticComponent<IconProps & React.RefAttributes<Icon>>;
+  label: string;
+}
+
+export const NavIcon: FC<NavIconProps> = (props) => {
+  const { to, Icon, label } = props;
+
+  return (
+    <NavLink to={to}>
+      <ActionIcon variant="filled" size="xl" radius="xl" aria-label={label}>
+        <Icon style={{ width: '70%', height: '70%' }} stroke={1.5} />
+      </ActionIcon>
+    </NavLink>
+  );
+};
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,22 +1,25 @@
-import { Button, Group } from '@mantine/core';
+import { ActionIcon, Button, Group } from '@mantine/core';
 import type { FC } from 'react';
 import { NavLink, Outlet } from 'react-router';
+import {
+  IconAdjustments,
+  IconBarbell,
+  IconChartLine,
+  IconClipboardList,
+  IconSettings,
+} from '@tabler/icons-react';
+import { NavIcon } from '../components/NavIcon';
 
 export const Layout: FC = () => {
   return (
     <>
       <Outlet />
-      <div className="absolute bottom-0 w-full p-2">
-        <Group justify="center" grow>
-          <Button component={NavLink} to="/" variant="outline">
-            Home
-          </Button>
-          <Button component={NavLink} to="/info" variant="outline">
-            Info
-          </Button>
-          <Button component={NavLink} to="/settings" variant="outline">
-            Settings
-          </Button>
+      <div className="absolute bottom-[20px] w-full">
+        <Group justify="center">
+          <NavIcon to="/" Icon={IconBarbell} label="Train" />
+          <NavIcon to="/analytics" Icon={IconChartLine} label="Analytics" />
+          <NavIcon to="/info" Icon={IconClipboardList} label="Info" />
+          <NavIcon to="/settings" Icon={IconSettings} label="Settings" />
         </Group>
       </div>
     </>
