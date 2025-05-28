818b1ef555374bfe795d4fa4f28417e9b1c6520d
zjcrumpton <zjcrumpton@gmail.com>
2025-05-27 19:09:21 -0600

    add basic info type

---
+++ src/features/settings/types/BasicInfo.ts
@@ -0,0 +1,10 @@
+export interface BasicInfo {
+  id: string;
+  created_at: string;
+  user_id: string;
+  name: string;
+  display_name: string;
+  dob: Date;
+  height_feet: number;
+  height_inches: number;
+}
