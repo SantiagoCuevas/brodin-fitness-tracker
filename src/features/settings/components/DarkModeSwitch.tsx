69061648009bf5e874f1c32bb668e151269d4b1d
zjcrumpton <zjcrumpton@gmail.com>
2025-05-27 18:53:16 -0600

    fix toggle value

--- src/features/settings/components/DarkModeSwitch.tsx
+++ src/features/settings/components/DarkModeSwitch.tsx
@@ -2,12 +2,18 @@ import { Switch, useMantineColorScheme } from '@mantine/core';
 
 export const DarkModeSwitch = () => {
   const { colorScheme, setColorScheme } = useMantineColorScheme();
+  const prefersDarkScheme =
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const isDark =
+    (colorScheme === 'auto' && prefersDarkScheme) || colorScheme === 'dark';
+
   return (
     <Switch
-      checked={colorScheme === 'dark'}
+      checked={isDark}
       label="Dark Mode"
       size="lg"
-      onClick={() => setColorScheme(colorScheme === 'dark' ? 'light' : 'dark')}
+      onClick={() => setColorScheme(isDark ? 'light' : 'dark')}
     />
   );
 };
