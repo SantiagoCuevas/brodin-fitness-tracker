7005a2e0ab3d294ba3486f15c894e89948a22aed
Santiago Cuevas <santiagocuevas77@gmail.com>
2025-05-28 21:40:58 -0600

    create add snapshot button

--- src/app/pages/PhysiqueTracking.tsx
+++ src/app/pages/PhysiqueTracking.tsx
@@ -1,5 +1,19 @@
+import { Button, Modal, Title } from '@mantine/core';
 import { MainPage } from '../../components/MainPage';
+import { useDisclosure } from '@mantine/hooks';
 
 export const PhysiqueTracking = () => {
-  return <MainPage title="Physique Tracking"></MainPage>;
+  const [opened, { open, close }] = useDisclosure(false);
+  return (
+    <MainPage title="Physique Tracking">
+      <Modal
+        opened={opened}
+        onClose={close}
+        title={'Add Snapshot'}
+        centered
+      ></Modal>
+      <Button onClick={open}>Add Snapshot</Button>
+      <Title order={2}>Snapshots</Title>
+    </MainPage>
+  );
 };
