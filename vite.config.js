12db4d3ca19a79d80a55be0246a5040d3a47d529
ValorousPete <valorouspete@gmail.com>
2025-05-28 20:35:03 -0600

    added build info to gitignore, updated tsconfig for dev builds

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [tailwindcss(), react()],
+    resolve: {
+        alias: {
+            // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+            '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+        },
+    },
+});
