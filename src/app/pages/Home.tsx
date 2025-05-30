d72a996ba2a2a0a45d6098c5ae90fd0c1c8d004a
zjcrumpton <zjcrumpton@gmail.com>
2025-05-30 17:28:31 -0600

    move progress pics to progress page

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,14 +1,5 @@
-import { useDisclosure } from '@mantine/hooks';
 import { MainPage } from '../../components/MainPage';
-import { ProgressPicModal } from '../../features/progress-pics/components/ProgressPicModal';
-import { Button } from '@mantine/core';
 
 export const Home = () => {
-  const [opened, { open, close }] = useDisclosure();
-  return (
-    <MainPage title="Home">
-      <ProgressPicModal opened={opened} close={close} />
-      <Button onClick={open}>Progress Pics</Button>
-    </MainPage>
-  );
+  return <MainPage title="Home"></MainPage>;
 };
