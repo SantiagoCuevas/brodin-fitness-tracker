eb8a3a15a4da716e2418e24ffbe765438a5697d0
Santiago Cuevas <santiagocuevas77@gmail.com>
2025-06-01 20:34:39 -0600

    create PhysiqueSnapshotForm types

---
+++ src/features/settings/types/PhysiqueSnapshotForm.ts
@@ -0,0 +1,15 @@
+export interface PhysiqueSnapshotForm {
+  notes: string;
+  weight: number;
+  BodyFat: number;
+  chest: number;
+  waist: number;
+  hip: number;
+  leftBicep: number;
+  rightBicep: number;
+  leftThigh: number;
+  rightThigh: number;
+  leftCalf: number;
+  rightCalf: number;
+  neck: number;
+}
